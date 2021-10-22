fd0bfaf85a1e7a379cf282e03685bc7f8a5bbead
mokodoko <forever7631@gmail.com>
2021-10-22 14:40:25 +0900

    Aws test

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,9 +5,8 @@ export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
-export SERVER_PORT =$(aws ssm get-parameters --region ap-northeast-2 --names SERVER_PORT --query Parameters[0].Value | sed 's/"//g')
+export SERVER_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names SERVER_PORT --query Parameters[0].Value | sed 's/"//g')
 
-authbind --deep pm2 start app.js
 
 
 authbind --deep pm2 start /dist/app.js
--- server/dist/app.js
+++ server/dist/app.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var dotenv = require('dotenv');
 var app = (0, express_1.default)();
 app.get("/", function (req, res) {
-    res.send("start aasdasd");
+    res.send([process.env.DATABASE_HOST, process.env.DATABASE_USER,
+        process.env.DATABASE_PASSWORD, process.env.DATABASE_PORT]);
 });
 app.listen(1234, function () {
     console.log("Hello, World!");

833b569a08fe91bc70a26ff4d1cbc4d3655b002d
mokodoko <forever7631@gmail.com>
2021-10-22 13:20:18 +0900

    Fix #2 Aws Setting

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,14 @@
 //const express = require('express');
 import express from "express"; 
+const dotenv = require('dotenv');
+//import dotenv = from "dotenv"
 const app = express(); 
 
 
 
 app.get("/",(req : express.Request , res : express.Response) =>{
-  res.send("start aasdasd,",process.env.DATABASE_HOST,process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,process.env.DATABASE_PORT);
+  res.send([process.env.DATABASE_HOST,process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,process.env.DATABASE_PORT]);
 })
 
 app.listen(1234, () => {

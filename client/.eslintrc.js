6b2d660e8edc0aefcbf5b2b473c07eeaba26ac89
mokodoko <forever7631@gmail.com>
2021-10-22 10:28:39 +0900

    test3

---
+++ client/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"][
+    "eslint:recommended", "airbnb-base", "plugin:prettier/recommended"
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+};

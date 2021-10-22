062469cbd2c7b8ddf94e272af8b6beb376107972
mokodoko <forever7631@gmail.com>
2021-10-22 22:23:36 +0900

    Aws path test2

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,8 @@ app.get("/",(req : express.Request , res : express.Response) =>{
   res.send(`H, host = ${process.env.DATABASE_HOST}
   DBuser = ${process.env.DATABASE_USER}
   DBpassword = ${process.env.DATABASE_PASSWORD}
-  ServerPort = ${process.env.DATABASE_PORT,process.env.SERVER_PORT}`);
+  DBPort = ${process.env.DATABASE_PORT}
+  ServerPort ${process.env.SERVER_PORT}`);
 })
 
 app.listen(80, () => {

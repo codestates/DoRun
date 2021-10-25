424cb36c9df53b426882a8a8df919e005c86f121
raonzane <raonzane@gmail.com>
2021-10-25 10:44:58 +0900

    임시 #7 LoginPage

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,76 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import Footer from '../../components/Footer/Footer';
 
-export const LoginPage = () => {
+
+
+const LoginPage = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [visibility, setVisibility] = useState('password');
+
+    const isVisible = () => {
+        if(visibility === 'password') {
+            setVisibility('text');
+        }
+        else {
+            setVisibility('password');
+        }
+    }
+
+    const onSubmitHandler = (e) => {
+
+    }
+
     return (
-        <div>
+        <>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'}}>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
 
-        </div>
+                <div>X</div>
+                <h1>Login</h1>
+
+                <br />
+
+                <div>유효성 검사 메시지</div>
+
+                <br />
+
+                <div>
+                    <input 
+                        type="email" 
+                        value={email}
+                        placeholder='Email'
+                        onChange={(e) => setEmail(e.target.value)} 
+                    />
+                </div>
+                <div>
+                    <input 
+                        type={visibility}
+                        value={password} 
+                        placeholder='Password'
+                        onChange={(e) => setPassword(e.target.value)} 
+                    />
+                    <button onClick={isVisible}>show</button>
+                </div>
+
+                <br />
+                <div>Forgot your password?</div>
+                <button onClick={onSubmitHandler}>Login</button>
+                <button >Google</button>
+                <button >Kakao</button>
+
+                <br />
+
+                <div>
+                    <span>아직 회원이 아니신가요?</span>
+                    <button>회원가입 하기</button>
+                </div>
+
+                </div>
+            </div>
+            <Footer />
+        </>
     )
 }
 

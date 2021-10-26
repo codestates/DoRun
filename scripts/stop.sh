48fac6b28fc5d2517ebf935c9230796e82019f48
mokodoko <forever7631@gmail.com>
2021-10-26 17:30:46 +0900

    Err feevents4

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/DoRun/server
-pm2 stop dist/app.js 2> /dev/null || true
-pm2 delete dist/app.js 2> /dev/null || true
+pm2 stop dist/src/index.js 2> /dev/null || true
+pm2 delete dist/src/index.js 2> /dev/null || true

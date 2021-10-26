cf70af9fa0eb356ed7f63a5b9745817e35914aaf
mokodoko <forever7631@gmail.com>
2021-10-26 17:58:28 +0900

    Err feevents5

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/DoRun/server
-pm2 stop dist/src/index.js 2> /dev/null || true
-pm2 delete dist/src/index.js 2> /dev/null || true
+pm2 stop dist/src/app.js 2> /dev/null || true
+pm2 delete dist/src/app.js 2> /dev/null || true
+pm2 ls

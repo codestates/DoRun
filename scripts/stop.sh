f07cbca7687eac8c4ee3220765ad5917cc098114
95mg <raininmate@gmail.com>
2021-10-27 21:06:29 +0900

    Chore: local file change accept

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/DoRun/server
-pm2 stop dist/app.js 2> /dev/null || true
-pm2 delete dist/app.js 2> /dev/null || true
+pm2 stop dist/index.js 2> /dev/null || true
+pm2 delete dist/index.js 2> /dev/null || true
+pm2 ls
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,39 +1,40 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
 import config from "../ormconfig";
-import * as express from 'express';
-import 'dotenv/config'
-import router from './routes'
-import * as cookieParser from 'cookie-parser';
-import * as cors from 'cors'
+import * as express from "express";
+import "dotenv/config";
+import router from "./routes";
+import * as cookieParser from "cookie-parser";
+import * as cors from "cors";
+import { User } from "./entity/User";
 
-
-const app = express(); 
+const app = express();
 
 createConnection(config)
-.then(() => {
-  console.log("DB CONNECTION!");
-})
-.catch((error) => {
-  console.log(error);
-});
+  .then(() => {
+    console.log("DB CONNECTION!", process.env.NODE_ENV);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  origin: true,
-  credentials: true,
-  methods: ['GET', 'POST', 'PATCH', 'DELETE']
-}));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  }),
+);
 
-app.use('/', router);
+app.use("/", router);
 
-
-app.get("/", (req : express.Request , res : express.Response) =>{
-    res.send("Hello")
-})
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello");
+});
 
 app.listen(process.env.SERVER_PORT, () => {
-  console.log(`listen Port = ${process.env.SERVER_PORT}`); 
+  console.log(`listen Port = ${process.env.SERVER_PORT}`);
 });

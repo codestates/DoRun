77d3642a275d289c3ff00772e07d1149998400a6
95mg <raininmate@gmail.com>
2021-10-27 21:07:56 +0900

    Chore: change accept server/scripts/stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/DoRun/server
-pm2 stop dist/index.js 2> /dev/null || true
-pm2 delete dist/index.js 2> /dev/null || true
+pm2 stop dist/src/index.js 2> /dev/null || true
+pm2 delete dist/src/index.js 2> /dev/null || true
 pm2 ls

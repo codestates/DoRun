7f2540756321c4c49807893fd59c0d5bce3413f7
raonzane <raonzane@gmail.com>
2021-10-29 13:27:36 +0900

    [Client] Fix: #17 Modal Css

--- client/src/pages/test.js
+++ client/src/pages/test.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './test.scss';
+import CrewModal from '../components/CrewModal/CrewModal';
 
 const test = () => {
-  return <div>test</div>;
+  const [crewModalPosition, setCrewModalPosition] = useState('down');
+  const crewModalHandler = () => {
+    crewModalPosition === 'down'
+      ? setCrewModalPosition('up')
+      : setCrewModalPosition('down');
+  };
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCrewModalPosition('up');
+        }}
+      >
+        crewModal
+      </button>
+      <div className={crewModalPosition}>
+        <CrewModal crewModalHandler={crewModalHandler} />
+      </div>
+    </div>
+  );
 };
 
 export default test;

8114bc5f858d5c458193c7dae54f26e660fbe303
mokodoko <forever7631@gmail.com>
2021-11-08 19:10:52 +0900

    [Server] Fix: git error.

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,42 +29,16 @@ app.use(
   })
 );
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "uploads/"); // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
-//   },
-//   filename(req, file, cb) {
-//     const ext = path.extname(file.originalname);
-//     console.log("file.originalname", file.originalname);
-//     cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
-//   },
-// });
-// const upload = multer({ storage: storage });
-
-// app.post("/img", upload.single("image"), (req: express.Request, res: express.Response) => {
-//   res.send("hello post");
-// });
-
 app.use("/", router);
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello");
 });
 
-// const server = require("http").createServer(app);
-// const io = require("socket.io")(server);
+const server = http.createServer(app);
 
-// server.listen(process.env.SERVER_PORT, () => {
-//   console.log(`listen Port = ${process.env.SERVER_PORT}`);
-// });
-
-const server = require("./soket");
-app.listen(process.env.SERVER_PORT, () => {
+server.listen(process.env.SERVER_PORT, () => {
   console.log(`listen Port = ${process.env.SERVER_PORT}`);
 });
 
-server.listen(4000, () => {
-  console.log(`Chating Port = 4000`);
-});
-
-export default app;
+socketInit(server);

8683896bf6ca2a61300ec7e4e1e60937d21bb704
mokodoko <forever7631@gmail.com>
2021-11-10 16:22:28 +0900

    test

--- server/src/controllers/crew.ts
+++ server/src/controllers/crew.ts
@@ -57,7 +57,7 @@ const CreateCrew = async (req: Request, res: Response) => {
 
 const EditCrew = async (req: Request, res: Response) => {
   try {
-    const { crewId }: any = req.params;
+    const crewId = Number(req.params.crewId);
     if (!crewId) return res.status(400).send();
 
     let crewInfo = await Crew.findOne({ id: crewId });
@@ -88,7 +88,7 @@ const EditCrew = async (req: Request, res: Response) => {
 };
 const InfoCrew = async (req: Request, res: Response) => {
   try {
-    const { crewId }: any = req.params;
+    const crewId = Number(req.params.crewId);
 
     const crewInfo = await Crew.findOne({ id: crewId });
     const CrewInUser = await User.find({

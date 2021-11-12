da4e8c469171786fe688752109764f82fdc5027d
chayezo <yejinchaa@naver.com>
2021-11-12 15:18:25 +0900

    [Client] Feat #174 add loading component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import LoginPage from './pages/LoginPage/LoginPage';
@@ -8,12 +8,23 @@ import MyPage from './pages/MyPage/MyPage';
 import ChatPage from './pages/ChatPage/ChatPage';
 // import Auth from './hoc/auth';
 import Nav from './components/Header/Nav';
+import Loading from './components/Loading/Loading';
 
 const App = () => {
   const accessToken = sessionStorage.getItem('accessToken');
   const userId = sessionStorage.getItem('userId');
   const userCrewId = sessionStorage.getItem('userCrewId');
   const userNickname = sessionStorage.getItem('userNickname');
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   useEffect(() => {
     console.log('accessToken: ' + accessToken);
     console.log('userId: ' + userId);
@@ -23,15 +34,21 @@ const App = () => {
 
   return (
     <>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/register" component={RegisterPage} />
-        <Route path="/map" component={MapPage} />
-        <Route path="/mypage" component={MyPage} />
-        <Route path="/chat" component={ChatPage} />
-      </Switch>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/register" component={RegisterPage} />
+            <Route path="/map" component={MapPage} />
+            <Route path="/mypage" component={MyPage} />
+            <Route path="/chat" component={ChatPage} />
+          </Switch>
+        </>
+      )}
     </>
   );
 };
